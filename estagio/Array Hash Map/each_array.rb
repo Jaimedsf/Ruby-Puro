5123bc1110c6825fed672afbd48da5a4e7e94673
Jaime da Silva <jaime_br@outlook.com>
2019-10-21 12:24:19 -0300

    alteracoes

--- estagio/Array Hash Map/each_array.rb
+++
@@ -1,9 +0,0 @@
-names = ['Joaozinho', 'Manoel', 'Juca']
-
-name = 'Leonardo Scorza'
-
-names.each do |name|
-    puts name  + ' sou eu'
-end
-
- # puts name
--- estagio/Conceitos Basicos/nomeidade.rb
+++
@@ -1,9 +0,0 @@
-
-print 'Digite seu nome: '
-name = gets.chomp.to_i
-
-print 'Digite sua idade: '
-idade = gets.chomp.to_i
- 
-
-puts "#{name} tem #{idade} anos!"
